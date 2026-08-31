9bd0815265bdf884b6585ff3a98f37f928cdf122
Megascope <megascope@kudesia.org>
2026-08-30 23:05:56 -0500

    use BIND_ADDR

--- llama.cpp/run.sh
+++ llama.cpp/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # assumes llm tool (uv tool install llm)
+
 source .env
 llm openai endpoint http://${BIND_ADDR}/v1 \
   -m "$(curl -s http://${BIND_ADDR}/v1/models | jq -r '.data[0].id')" \
-  --chat
+  "$*"
--- llama.cpp/test.sh
+++ llama.cpp/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # assumes llm tool (uv tool install llm)
-llm openai endpoint http://127.0.0.1:8001/v1 \
-  -m "$(curl -s http://127.0.0.1:8001/v1/models | jq -r '.data[0].id')" \
+source .env
+llm openai endpoint http://${BIND_ADDR}/v1 \
+  -m "$(curl -s http://${BIND_ADDR}/v1/models | jq -r '.data[0].id')" \
   "a woman walking through London in heavy rain"
